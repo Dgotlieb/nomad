05e5538eab2e0a1fe47730adcff27c0bbab62b26
Michele Degges <mdeggies@gmail.com>
2022-03-23 16:49:54 -0700

    patch

--- scripts/ci-install-buf.sh
+++ scripts/ci-install-buf.sh
@@ -9,15 +9,14 @@ DOWNLOAD=https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf
 # Install buf based on goos
 if [ $1 = "windows" ]; then
   DOWNLOAD="${DOWNLOAD}-Windows-x86_64.exe"
-  curl -sSL --fail "${DOWNLOAD}" -o /bin/buf
+  curl -sSL --fail "${DOWNLOAD}" -o /tmp/buf.exe
 elif [ $1 = "darwin" ]; then
-  DOWNLOAD="${DOWNLOAD}-Darwin-x86_64"
-  curl -sSL --fail "${DOWNLOAD}" -o /bin/buf
+  DOWNLOAD="${DOWNLOAD}-Darwin-x86_64.tar.gz"
+  wget -c "${DOWNLOAD}" -O - | tar -xz -C /tmp
 else
-    DOWNLOAD="${DOWNLOAD}-Linux-x86_64"
-    curl -sSL --fail "${DOWNLOAD}" -o /bin/buf
+    DOWNLOAD="${DOWNLOAD}-Linux-x86_64.tar.gz"
+    wget -c "${DOWNLOAD}" -O - | tar -xz -C /tmp
 fi
 
 # Simple smoke test to ensure buf is installed
-chmod +x /bin/buf
 buf --version
