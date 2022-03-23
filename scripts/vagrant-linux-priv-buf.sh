fa28ca561c7bf5e9fcd115d76c41cf4535851890
Michele Degges <mdeggies@gmail.com>
2022-03-23 14:37:27 -0700

    Fix

--- scripts/vagrant-linux-priv-buf.sh
+++ scripts/vagrant-linux-priv-buf.sh
@@ -8,25 +8,19 @@ DOWNLOAD=https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-Linux
 
 function install() {
   if command -v buf >/dev/null; then
-    if [[ "${VERSION}" = "$(buf  --version)" ]] ; then
+    if [ "${VERSION}" = "$(buf  --version)" ] ; then
       return
     fi
   fi
 
-  # Install to a different locatin when called from GHA runners
-  if [[ $1 == "GHA_RUNNERS" ]]; then
-    # Download
-    curl -sSL --fail "$DOWNLOAD" -o $RUNNER_WORKSPACE/$(basename $GITHUB_REPOSITORY)/bin/buf
-    # Make executable
-    chmod +x $RUNNER_WORKSPACE/$(basename $GITHUB_REPOSITORY)/bin/buf
-  else
-    # Download
-    curl -sSL --fail "$DOWNLOAD" -o /tmp/buf
-    # Make executable
-    chmod +x /tmp/buf
-    # Move buf to /usr/bin
-    mv /tmp/buf /usr/bin/buf
-  fi
+  # Download
+  curl -sSL --fail "$DOWNLOAD" -o /tmp/buf
+
+  # make executable
+  chmod +x /tmp/buf
+
+  # Move buf to /usr/bin
+  mv /tmp/buf /usr/bin/buf
 }
 
 install
