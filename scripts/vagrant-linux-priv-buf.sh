d68ce264599aaeae5c40608e3747e51b2b217a2a
Michele Degges <mdeggies@gmail.com>
2022-03-23 14:32:29 -0700

    Testing

--- scripts/vagrant-linux-priv-buf.sh
+++ scripts/vagrant-linux-priv-buf.sh
@@ -3,24 +3,30 @@
 set -o errexit
 
 # Make sure you grab the latest version
-VERSION=0.36.0
+VERSION=1.1.1
 DOWNLOAD=https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-Linux-x86_64
 
 function install() {
   if command -v buf >/dev/null; then
-    if [ "${VERSION}" = "$(buf  --version)" ] ; then
+    if [[ "${VERSION}" = "$(buf  --version)" ]] ; then
       return
     fi
   fi
 
-  # Download
-  curl -sSL --fail "$DOWNLOAD" -o /tmp/buf
-
-  # make executable
-  chmod +x /tmp/buf
-
-  # Move buf to /usr/bin
-  mv /tmp/buf /usr/bin/buf
+  # Install to a different locatin when called from GHA runners
+  if [[ $1 == "GHA_RUNNERS" ]]; then
+    # Download
+    curl -sSL --fail "$DOWNLOAD" -o $RUNNER_WORKSPACE/$(basename $GITHUB_REPOSITORY)/bin/buf
+    # Make executable
+    chmod +x $RUNNER_WORKSPACE/$(basename $GITHUB_REPOSITORY)/bin/buf
+  else
+    # Download
+    curl -sSL --fail "$DOWNLOAD" -o /tmp/buf
+    # Make executable
+    chmod +x /tmp/buf
+    # Move buf to /usr/bin
+    mv /tmp/buf /usr/bin/buf
+  fi
 }
 
 install
