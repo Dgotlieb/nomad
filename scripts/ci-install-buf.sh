16ff36f6e8857399ec57ff5c2f5b941ed1b16bbe
Michele Degges <mdeggies@gmail.com>
2022-03-23 16:04:33 -0700

    Try again

--- scripts/ci-install-buf.sh
+++ scripts/ci-install-buf.sh
@@ -4,17 +4,17 @@ echo "Installing dependencies for goos:$1"
 
 # Make sure you grab the latest version
 VERSION=1.1.1
-DOWNLOAD="https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-"
+DOWNLOAD=https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf
 
 # Install buf based on goos
 if [[ $1 == "windows" ]]; then
-  DOWNLOAD+="Windows-x86_64.exe"
+  DOWNLOAD="${DOWNLOAD}-Windows-x86_64.exe"
   curl -sSL --fail "$DOWNLOAD" -o /tmp/buf
 elif [[ $1 == "darwin" ]]; then
-  DOWNLOAD+="Darwin-x86_64"
+  DOWNLOAD="${DOWNLOAD}-Darwin-x86_64"
   curl -sSL --fail "$DOWNLOAD" -o /tmp/buf
 else
-    DOWNLOAD+="Linux-x86_64"
+    DOWNLOAD="${DOWNLOAD}-Linux-x86_64"
     curl -sSL --fail "$DOWNLOAD" -o /tmp/buf
 fi
 
