ce005eefbbc5ef6d966fa39cce7bdb96e98f41d5
Michele Degges <mdeggies@gmail.com>
2022-03-23 18:59:27 -0700

    fix

--- scripts/ci-install-deps.sh
+++ scripts/ci-install-deps.sh
@@ -11,24 +11,19 @@ if [ $1 = "windows" ]; then
   DOWNLOAD="${DOWNLOAD}-Windows-x86_64.exe"
   # In GHA, $HOME evaluates to /home/runner
   wget --quiet "${DOWNLOAD}" -O "$HOME/.local/bin/buf.exe"
-  chmod +x $HOME/.local/bin/buf.exe
 elif [ $1 = "darwin" ]; then
   DOWNLOAD="${DOWNLOAD}-Darwin-x86_64.tar.gz"
   wget --quiet "${DOWNLOAD}" -O - | tar -xz -C /tmp
   # In GHA, $HOME evaluates to /home/runner
   mv /tmp/buf/bin/buf "$HOME/.local/bin"
-  chmod +x $HOME/.local/bin/buf
+  # Exit script; nothing more to do for darwin builds
   exit 0
 else
   DOWNLOAD="${DOWNLOAD}-Linux-x86_64.tar.gz"
   wget --quiet "${DOWNLOAD}" -O - | tar -xz -C /tmp
   # In GHA, $HOME evaluates to /home/runner
   mv /tmp/buf/bin/buf "$HOME/.local/bin"
-  chmod +x $HOME/.local/bin/buf
 fi
-
-# Simple smoke test to ensure buf's installed
-buf --version
 
 #### Install required libraries ####
 
