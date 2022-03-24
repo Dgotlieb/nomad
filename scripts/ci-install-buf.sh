ca20fa0e7bb2f3c93b98acc4f524f0341702ba83
Michele Degges <mdeggies@gmail.com>
2022-03-23 18:38:43 -0700

    Testing with linux

--- scripts/ci-install-buf.sh
+++ scripts/ci-install-buf.sh
@@ -14,14 +14,16 @@ echo "what is home: $HOME"
 # Install buf based on goos
 if [ $1 = "windows" ]; then
   DOWNLOAD="${DOWNLOAD}-Windows-x86_64.exe"
-  wget --quiet "${DOWNLOAD}" -O $PWD/tmp/buf.exe
-  chmod +x $PWD/tmp/buf.exe
+  wget --quiet "${DOWNLOAD}" -O "$HOME/.local/bin/buf.exe"
+  chmod +x $HOME/.local/bin/buf.exe
 elif [ $1 = "darwin" ]; then
   DOWNLOAD="${DOWNLOAD}-Darwin-x86_64.tar.gz"
-  wget --quiet "${DOWNLOAD}" -O - | tar -xz -C $PWD/tmp
-  chmod +x $PWD/tmp/buf/bin/buf
+  wget --quiet "${DOWNLOAD}" -O - | tar -xz -C /tmp
+  mv /tmp/buf/bin/buf "$HOME/.local/bin"
+  chmod +x $HOME/.local/bin/buf
 else
   DOWNLOAD="${DOWNLOAD}-Linux-x86_64.tar.gz"
-  wget --quiet "${DOWNLOAD}" -O - | tar -xz -C $PWD/tmp
-  chmod +x $PWD/tmp/buf/bin/buf
+  wget --quiet "${DOWNLOAD}" -O - | tar -xz -C /tmp
+  mv /tmp/buf/bin/buf "$HOME/.local/bin"
+  chmod +x $HOME/.local/bin/buf
 fi
