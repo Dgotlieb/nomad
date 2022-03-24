dd4991babbcb37aa7cfce376a25704af1def49b2
Michele Degges <mdeggies@gmail.com>
2022-03-23 17:44:35 -0700

    test linux

--- scripts/ci-install-buf.sh
+++ scripts/ci-install-buf.sh
@@ -6,22 +6,21 @@ echo "Installing dependencies for goos:$1"
 VERSION=1.1.1
 DOWNLOAD=https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf
 
+echo "where am i: $PWD"
+
 # Install buf based on goos
 if [ $1 = "windows" ]; then
   DOWNLOAD="${DOWNLOAD}-Windows-x86_64.exe"
   wget --quiet "${DOWNLOAD}" -O /tmp/buf.exe
   chmod +x /tmp/buf.exe
-  ls /tmp
 elif [ $1 = "darwin" ]; then
   DOWNLOAD="${DOWNLOAD}-Darwin-x86_64.tar.gz"
   wget --quiet "${DOWNLOAD}" -O - | tar -xz -C /tmp
   chmod +x /tmp/buf/bin/buf
-  ls /tmp
 else
   DOWNLOAD="${DOWNLOAD}-Linux-x86_64.tar.gz"
   wget --quiet "${DOWNLOAD}" -O - | tar -xz -C /tmp
   chmod +x /tmp/buf/bin/buf
-  ls /tmp
 fi
 
 # Simple smoke test to ensure buf is installed
